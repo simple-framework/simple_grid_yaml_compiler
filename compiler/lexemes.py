aef1f8d0112b01874d711b82519219c920b9e82f
Micky <yun_chan4@hotmail.com>
2019-03-22 22:56:23 +0200

    add test for lexemes

--- compiler/lexemes.py
+++ compiler/lexemes.py
@@ -1,6 +1,6 @@
 import re
-from ruamel.yaml import YAML
 from ruamel.yaml.comments import CommentedSeq, CommentedMap
+
 
 def get_repo_list(site_level_configuration_file):
     urls = []
@@ -11,6 +11,7 @@ def get_repo_list(site_level_configuration_file):
             url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url_line_string)
             urls.append(url[0])
     return urls
+
 
 ##########################################################################
 # Warning: Badass recursive way to replace all __from__ keywords correctly
@@ -29,7 +30,7 @@ def parse_for_variable_hierarchies(data, keyword):
         for sub_data in data:
             sub_data_updated = parse_for_variable_hierarchies(sub_data, keyword)
             data_updated.append(sub_data_updated)
-        return  data_updated
+        return data_updated
     elif data_type is CommentedMap:
         data_updated = CommentedMap()
         for sub_data_key in data:
--- tests/tests_lexemes.py
+++ tests/tests_lexemes.py
@@ -1,19 +1,32 @@
-from compiler.lexemes import get_repo_list
+from compiler.lexemes import get_repo_list, parse_for_variable_hierarchies
+from ruamel.yaml.comments import CommentedSeq, CommentedMap
 import unittest
 
-class MyTest(unittest.TestCase):
-    def test(self):
-        input = """
-        lightweight_components:
-  - type: compute_element
-    repository_url: "https://github.com/WLCG-Lightweight-Sites/wlcg_lightweight_site_ce_cream"
-    nodes:
-      - node: lw-site-droplet-0
-        container_count: 1
-      - node: ec2-18-184-37-92.eu-central-1
-        container_count: 2
-    preferred_tech_stack:
-      level_2_configuration: yaim
-      """    
 
-        self.assertEqual(input, ["https://github.com/WLCG-Lightweight-Sites/wlcg_lightweight_site_ce_cream"])
+class TestLexemes(unittest.TestCase):
+    def test_get_repo_list(self):
+        input = open('./tests/data/lexemes_test', "r")
+        self.assertEqual(get_repo_list(input), ["https://github.com/WLCG-Lightweight-Sites/wlcg_lightweight_site_ce_cream"])
+
+    def test_parse_for_variable_hierarchies(self):
+        data = 'testStr'
+        keyword = 'Key'
+        self.assertEqual(data, parse_for_variable_hierarchies(data, keyword))
+
+        data = CommentedSeq()
+        data.insert(0, 'First')
+        data.insert(1, 'Second')
+        data.insert(2, 'Third')
+        data.insert(3, 'Key')
+        self.assertEqual('Key', parse_for_variable_hierarchies(data, keyword))
+
+        data = CommentedMap()
+        data.insert(0, 'First', 'test1')
+        data.insert(1, 'Second', 'test2')
+        data.insert(2, 'Third', 'test3')
+        data.insert(3, 'Key', 'test4')
+        self.assertEqual('test4', parse_for_variable_hierarchies(data, keyword))
+
+
+if __name__ == '__main__':
+    unittest.main()
